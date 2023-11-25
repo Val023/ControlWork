28dd42dd224f93894b33bc2e93f48f516c7a11cc
Valentina <vdronova938@gmail.com>
2023-11-25 14:46:58 +0300

    создаём метод void

--- Program.cs
+++ Program.cs
@@ -2,3 +2,4 @@
 Console.Write("Введите количество элементов массива: ");
 int temp = Convert.ToInt32(Console.ReadLine());
 string [] stringArray = new string [temp];
+void Array(string [] stringArray)
