619af8685531b5c42339ec6459b082e6f164fd8b
Valentina <vdronova938@gmail.com>
2023-11-25 14:49:12 +0300

    Создаём вывод текста для пользователя о вводе элементов массива

--- Program.cs
+++ Program.cs
@@ -5,5 +5,9 @@ string [] stringArray = new string [temp];
 void Array(string [] stringArray)
 {
     for (int i = 0;i<stringArray.Length;i++)
+    {
+        Console.WriteLine($"Введите {i+1} элемент массива");
+        stringArray[i] = Console.ReadLine();
+    }
 }
 
